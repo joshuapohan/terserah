af0c43f3c4debb39fe3fdb26a91ddc3c58a09127
senel34 <joshuapohan93@gmail.com>
2019-01-28 00:02:07 +0700

    fix port issue

--- server/server.js
+++ server/server.js
@@ -25,6 +25,6 @@ io.on('connection', (socket)=>{
 	});
 });
 
-server.listen(process.env.port, ()=>{
+server.listen(process.env.PORT || 3000, ()=>{
 	console.log('Server started on port ' + process.env.port);
 });
