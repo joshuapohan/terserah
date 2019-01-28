4fea4aee8936c6e16a8a1fc35c6ef860ee447605
senel34 <joshuapohan93@gmail.com>
2019-01-28 22:16:43 +0700

    use React

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,5 @@
 var socket = io();
 
 socket.on('connect', function(){
-	// alert('connected');
-});
 
-var randomFoodButton = jQuery('#random-food-btn');
-
-randomFoodButton.on('click', function(){
-	if(!navigator.geolocation){
-		return alert('Geolocation not supported by your browser');
-	}
-	randomFoodButton.attr('disabled', 'disabled').text('Getting Food');
-	navigator.geolocation.getCurrentPosition(function(position){
-		socket.emit('getRandomFood', {
-			latitude: position.coords.latitude,
-			longitude: position.coords.longitude
-		}, function(restaurant){
-			var res_name = jQuery('#restaurant-name');
-			var address = jQuery('#restaurant-address');
-			res_name.text(restaurant.name);
-			address.text(restaurant.location.address);
-			randomFoodButton.removeAttr('disabled').text('Terserah');
-		});
-	}, function(){
-		alert("Unable to fetch location");
-	});
 });
