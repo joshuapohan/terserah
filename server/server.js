937420f5ce4f0543b4d2787e63bb858290223a33
senel34 <joshuapohan93@gmail.com>
2019-01-29 22:45:00 +0700

    added more info button, calls zomato api to get more details, add favicon

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const favicon = require('serve-favicon')
 
-const {getRandomRestaurant} = require('./utils/api');
+const {getRandomRestaurant, getRestaurantDetails} = require('./utils/api');
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
@@ -11,6 +12,7 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+app.use(favicon(publicPath + '/images/favicon.ico'));
 
 app.get('/test', (req, res)=>{
 	res.status(200).sendFile(publicPath + '/index.html');
@@ -21,6 +23,13 @@ io.on('connection', (socket)=>{
 	socket.on('getRandomFood', (location, callback)=>{
 		getRandomRestaurant(location.latitude + ',' + location.longitude).then((restaurant)=>{
 			callback(restaurant);
+		});
+	});
+
+
+	socket.on('getMoreDetails', (restaurant, callback)=>{
+		getRestaurantDetails(restaurant).then((restaurant_detail)=>{
+			callback(restaurant_detail);
 		});
 	});
 });
--- server/utils/api.js
+++ server/utils/api.js
@@ -20,19 +20,63 @@ async function getNearbyVenues(ll){
 	}
 }
 
+function getTempNearbyLocation(ll){
+	//iterate through stored list, check if within radius
+	let lat = parseFloat(ll.split(',')[0]);
+	let long = parseFloat(ll.split(',')[1]);
+
+	for(var latlong in tmpList){
+		let lat_ref = parseFloat(latlong.split(',')[0]);
+		let long_ref = parseFloat(latlong.split(',')[1]);
+		let radius =  ((lat - lat_ref) ** 2 + (long - long_ref) ** 2 ) ** 0.5;
+
+		//current threshold is around 500m
+		if(radius < 0.005){
+			return tmpList[latlong];
+		}
+	}
+}
+
 async function getRandomRestaurant(location){
 	const ll = location;
 
-	const restaurants = await getNearbyVenues(ll).then((res)=>{
-		const venues = res.data.response.groups[0].items.map((venue)=>{
-			return _.pick(venue.venue,['name', 'location']);
-		});
-		tmpList[ll] = venues;
-		return venues;
-	}).catch((e)=>console.log(e));
+	//check if location is within radius of existing stored location first
+	let restaurants = getTempNearbyLocation(ll);
+
+	if(typeof restaurants === 'undefined'){
+		try{
+			const api_res = await getNearbyVenues(ll);
+			restaurants =  api_res.data.response.groups[0].items.map((venue)=>{
+					return _.pick(venue.venue,['name', 'location']);
+				});
+			tmpList[ll] = restaurants;
+		} 
+		catch(e){
+			console.log(e);
+		}
+	}
 
 	const rand = getRndInteger(0, restaurants.length - 1);
+	//
+	getRestaurantDetails(restaurants[rand]);
+	//
 	return restaurants[rand];
+}
+
+async function getRestaurantDetails(restaurant){
+	const api_key = process.env.ZOMATO_KEY || process.env.zomato_key;
+	const lat = restaurant.location.lat;
+	const lon = restaurant.location.lng;
+	const name = encodeURI(restaurant.name);
+	const requestURL = 'https://developers.zomato.com/api/v2.1/search?q='+ name +'&count=1&lat=' + lat + '&lon=' + lon + '&radius=100';
+	const res = await axios.get(requestURL, {
+		headers:{
+			'user-key': api_key
+		}
+	});
+	if(res && res.data && res.data.restaurants[0]){
+		return 	_.pick(res.data.restaurants[0].restaurant, ['name', 'url', 'cuisines', 'thumb']);
+	}
 }
 
 // getRandomRestaurant('-6.1649353,106.79182650000001').then((restaurant)=>{
@@ -40,5 +84,6 @@ async function getRandomRestaurant(location){
 // }).catch((e)=>console.log(e));
 
 module.exports = {
-	getRandomRestaurant
+	getRandomRestaurant,
+	getRestaurantDetails
 };
